55680b173e151bdc00547d5bb1cfee7dbe77fd47
Marpami <mirka.palojarvi@gameteam.dev>
2022-08-03 19:42:24 +0300

    new comment
    :O

--- MixAndJam Project/Assets/Scripts/GameController.cs
+++ MixAndJam Project/Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Mirka
+        //Mirka was here!!!
     }
 
     // Update is called once per frame
