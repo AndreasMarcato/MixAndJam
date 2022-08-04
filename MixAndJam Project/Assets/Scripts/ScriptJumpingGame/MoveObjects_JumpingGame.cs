5b6bf54a59a50f53841a5d3ebed8700ec4cf3168
Marpami <mirka.palojarvi@gameteam.dev>
2022-08-04 22:13:09 +0300

    Broken / Added 3D Character
    Some things are a bit broken. Don't worry I'll fix it later

--- MixAndJam Project/Assets/Scripts/ScriptJumpingGame/MoveObjects_JumpingGame.cs
+++ MixAndJam Project/Assets/Scripts/ScriptJumpingGame/MoveObjects_JumpingGame.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class MoveObjects_JumpingGame : MonoBehaviour
 {
-    private Rigidbody2D rb;
+   // private Rigidbody2D rb;
+    private Rigidbody rb3D;
 
     private void Awake()
     {
         //--Get the rigidbody
-        rb = GetComponent<Rigidbody2D>();
+     //   rb = GetComponent<Rigidbody2D>();
+        rb3D = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        //--Moves the object
-        rb.velocity = new Vector2(-2f, 0f);
+        //--Moves the 2D object
+     //   rb.velocity = new Vector2(-2f, 0f);
+        //--Moves the 3D object
+        rb3D.velocity = new Vector3(-2, 0f, 0f);
     }
 
     //--Destroy this object if hits the object with tag "Destroy"
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Destroy"))
-        {
-            Destroy(this.gameObject);
-        }
-    }
+  //  private void OnCollisionEnter2D(Collision2D collision)
+  //  {
+  //      if (collision.collider.CompareTag("Destroy"))
+  //      {
+  //          Destroy(this.gameObject);
+  //      }
+  //  }
 }
