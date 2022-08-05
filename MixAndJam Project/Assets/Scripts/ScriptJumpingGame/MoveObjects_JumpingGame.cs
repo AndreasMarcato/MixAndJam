5f19cfa40a6d27743a729e36f31d12f9ca22fc09
Marpami <mirka.palojarvi@gameteam.dev>
2022-08-05 10:54:11 +0300

    Now 3D Model Friendly
    Like I said 3D friendly. Now works with player having 3D model.

--- MixAndJam Project/Assets/Scripts/ScriptJumpingGame/MoveObjects_JumpingGame.cs
+++ MixAndJam Project/Assets/Scripts/ScriptJumpingGame/MoveObjects_JumpingGame.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class MoveObjects_JumpingGame : MonoBehaviour
 {
-   // private Rigidbody2D rb;
+    //-------THIS IS FOR MOVING AND DESTROYING 3D OBJECTS--------
     private Rigidbody rb3D;
 
     private void Awake()
     {
         //--Get the rigidbody
-     //   rb = GetComponent<Rigidbody2D>();
         rb3D = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        //--Moves the 2D object
-     //   rb.velocity = new Vector2(-2f, 0f);
         //--Moves the 3D object
         rb3D.velocity = new Vector3(-2, 0f, 0f);
     }
 
-    //--Destroy this object if hits the object with tag "Destroy"
-  //  private void OnCollisionEnter2D(Collision2D collision)
-  //  {
-  //      if (collision.collider.CompareTag("Destroy"))
-  //      {
-  //          Destroy(this.gameObject);
-  //      }
-  //  }
+
+
+    //--On collision destroy this object
+    private void OnCollisionEnter(Collision collision)
+    {
+              if (collision.collider.CompareTag("Destroy"))
+              {
+                  Destroy(this.gameObject);
+              }
+    }
 }
--- MixAndJam Project/Assets/Scripts/ScriptJumpingGame/PlayerMovement_JumpingGame.cs
+++ MixAndJam Project/Assets/Scripts/ScriptJumpingGame/PlayerMovement_JumpingGame.cs
@@ -9,16 +9,14 @@ public class PlayerMovement_JumpingGame : MonoBehaviour
     //--Drag and drop CheckGround in groundCheck
     //--Also give all the ground objects layer named Ground and choose that in Sorting Layer. That will be used to determine what is ground
 
-    //--Reference to rigidbody2D
-    public new Rigidbody2D rigidbody;
+    //--Reference to rigidbody 
+    public new Rigidbody rigidbody;
     //--Force applied to Rigidbody to make player jump
-    public float jumpForce = 1000;
+    public float jumpForce = 6;
 
     //--Check if player is on the ground
     private bool isGrounded;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
+
 
     //--Double Jump
     private int extraJumps;
@@ -34,13 +32,7 @@ public class PlayerMovement_JumpingGame : MonoBehaviour
         //--Sets jump into value it was given (1), meaning we get one extra jump and can double jump
         extraJumps = extraJumpValue;
         //-- Get rb
-        rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        //--Checking if player is on the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
 
@@ -57,19 +49,19 @@ public class PlayerMovement_JumpingGame : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && extraJumps > 0)
         {
                 //--Then apply the force to Rigidbody to make player jump
-                rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+                rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 //--when you are in the air and jump again you will lose your extra jump
                 extraJumps--;
         }
         else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && extraJumps == 0 && isGrounded == true)
         {
             //--if we are on the ground and jump
-            rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
     //--Game Over happens here if player hits object
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter(Collision collision)
     {
         //--If the object has GameOver tag, player loses
         if (collision.collider.CompareTag("GameOver"))
@@ -78,6 +70,21 @@ public class PlayerMovement_JumpingGame : MonoBehaviour
             GameController_JumpingGame.isGameOver = true;   //--Show Game Over screen. isGameOver is in GameController_JumpingGame script
             GameController_JumpingGame.isPaused = true;     //--Game is paused
             gCJumpingGame.pauseGame();                      //--Call the pause to happen
+        }
+
+        //--If player is on the ground
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //--If player is not on the ground
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
