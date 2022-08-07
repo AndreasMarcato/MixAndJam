fe7d7d1fa5f5287365749e7a78262e53c2484b9a
GrblackStar <hristiana_h@yahoo.com>
2022-08-08 02:06:55 +0300

    disappearing background

--- MixAndJam Project/Assets/Scripts/PiecesScriptPuzzleGame.cs
+++ MixAndJam Project/Assets/Scripts/PiecesScriptPuzzleGame.cs
@@ -10,64 +10,10 @@ public class PiecesScriptPuzzleGame : MonoBehaviour
 
 
 
-
-
-    /*
-    public GameObject background = null;
-    private SpriteRenderer sprite = null;
-    public Sprite starsBack = null;
-    
-    
-    
-    
-    // Start is called before the first frame update
     void Start()
     {
-        sprite = background.GetComponent<SpriteRenderer>();
+        Invoke("CreatingPieces", 6f);
 
-        Debug.Log("start");
-        
-        StartCoroutine(Timer(1.5f));
-        
-        Debug.Log("stop");
-        RightPosition = transform.position;
-        transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-4f, -3.5f));
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // snap system:
-        if (Vector3.Distance(transform.position, RightPosition) < 0.3f)
-        {
-            if (!Selected)
-            {
-                transform.position = RightPosition;
-                InRightPosition = true;
-            }
-            
-        }
-        
-    }
-
-
-    private IEnumerator Timer(float seconds)
-    {
-        Debug.Log("timer start");
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("timer end");
-        //sprite = starsBack;
-    }
-    */
-
-
-
-    void Start()
-    {
-        
-        RightPosition = transform.position;
-        transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-4f, -3.5f));
 
     }
 
@@ -84,5 +30,16 @@ public class PiecesScriptPuzzleGame : MonoBehaviour
 
         }
     }
+
+
+
+    private void CreatingPieces()
+    {
+
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-4f, -3.5f));
+    }
+
+
 
 }
