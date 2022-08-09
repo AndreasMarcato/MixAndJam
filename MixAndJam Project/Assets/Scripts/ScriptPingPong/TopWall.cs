aac042cd77d738957ab2f620b2680ba0b61ecf6d
Marpami <mirka.palojarvi@gameteam.dev>
2022-08-10 00:58:20 +0300

    All Games are now connected
    All games are now connected.
    All games can now be played from Game Selection screen.
    
    Missing:
    Main Menu!!

--- MixAndJam Project/Assets/Scripts/ScriptPingPong/TopWall.cs
+++ MixAndJam Project/Assets/Scripts/ScriptPingPong/TopWall.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class TopWall : Logic
 {
+    public Button nextButton;
+
     void OnTriggerEnter2D()
     {
         
@@ -20,6 +23,14 @@ public class TopWall : Logic
         //Mirka you can add the scene game to Load here;
         //or if you want to have a few seconds to load the next scene,
         //you can use the Invoke("WinCondition", seconds_to_wait_with_f) on line 13 instead of WinCondition();
-        SceneManager.LoadScene("Scene");
+        // SceneManager.LoadScene("Scene");              //--Btw the thing down is better
+        nextButton.gameObject.SetActive(true);           //--Activate and show the nextButton
+        Time.timeScale = 0;                              //--Stop the game once you win
+    }
+
+    //--Haha, this whole thing is needed to change scenes
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
