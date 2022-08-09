ce2aa1e37234ccb82d3bc24fcaa965932b2d06c9
noor-ain <checkmate9080@gmail.com>
2022-08-09 19:04:50 +0500

    updated

--- MixAndJam Project/Assets/Scripts/GameController.cs
+++ MixAndJam Project/Assets/Scripts/GameController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
-{
+{   
+    //
     // Start is called before the first frame update
     void Start()
     {
